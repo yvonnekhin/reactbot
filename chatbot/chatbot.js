2956ca1cb51e21fc04159eb6214e9079f8e4ea71
yvonnekhin <yvonnekhin@hotmail.com>
2020-05-08 18:47:05 +0800

    connect client side to dialogflow

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,21 +1,20 @@
-'use strict'
+'use strict';
 const dialogflow = require('dialogflow');
-const structjson = require('./structjson');
+const structjson = require('./structjson.js');
 const config = require('../config/keys');
 
-const projectID = config.googleProjectID;
+const projectId = config.googleProjectID;
+const sessionId = config.dialogFlowSessionID;
+const languageCode = config.dialogFlowSessionLanguageCode;
 const credentials = {
-    client_email: config.googleClientID,
-    private_key: config.googlePrivateKey
+    client_email: config.googleClientEmail,
+    private_key:
+    config.googlePrivateKey,
 };
-
-const sessionClient = new dialogflow.SessionsClient({projectID: projectID, credentials: credentials});
-
-const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
-
+const sessionClient = new dialogflow.SessionsClient({projectId, credentials});
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 module.exports = {
     textQuery: async function(text, parameters = {}) {
-
         // This is how we access another module exports method.
         let self = module.exports;
         const request = {
@@ -35,29 +34,29 @@ module.exports = {
         let responses = await sessionClient.detectIntent(request);
         responses = await self.handleAction(responses);
         return responses;
+
     },
 
     eventQuery: async function(event, parameters = {}) {
-        
-        // This is how we access another module exports method.
         let self = module.exports;
         const request = {
             session: sessionPath,
             queryInput: {
                 event: {
                     name: event,
-                    parameters: structjson.jsonToStructProto(parameters),
+                    parameters: structjson.jsonToStructProto(parameters), //Dialogflow's v2 API uses gRPC. You'll need a jsonToStructProto method to convert your JavaScript object to a proto struct.
                     languageCode: languageCode,
                 },
             }
         };
+
         let responses = await sessionClient.detectIntent(request);
         responses = await self.handleAction(responses);
         return responses;
     },
 
-    // handleAction is a function, that expects response and will for now, only return it.
+
     handleAction: function(responses){
         return responses;
-    }
+    },
 }
