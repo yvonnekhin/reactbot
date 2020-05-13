839b98bcd27c0dd0e88e49e2b1839a4bed5e5000
yvonnek2359 <yvonne.khin@2359media.com>
2020-05-13 21:34:03 +0800

    until lesson 74

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,4 @@ module.exports = {
     googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
     googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
     mongoURI: process.env.MONGO_URI,
-    //googlePrivateKey: JSON.parse(process.env.GOOGLE_PRIVATE_KEY),
 }
