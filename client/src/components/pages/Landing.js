65b6a4888d75291578b4daf22d6632d1d888460d
yvonnek2359 <yvonne.khin@2359media.com>
2020-05-14 13:44:57 +0800

    removed 'great' from Landing page

--- client/src/components/pages/Landing.js
+++ client/src/components/pages/Landing.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Landing = () => {
     return(
         <div style={{ textAlign: 'center' }}>
-            <h1>Selling you greate stuff</h1>
+            <h1>Selling you stuff</h1>
             with the help of the chatbot
         </div>
     )
